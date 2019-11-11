74266126f1ea477faba7b4c328353f03392bfb39
nlitkowski <norbert.litkowski@gmail.com>
2019-11-11 16:03:03 +0100

    Working text aligning

--- e/02.py
+++ e/02.py
@@ -9,18 +9,19 @@ def main():
         for y in text_json:
             if x != y and (x["site"]).replace(".php", "_en.php") == y["site"]:
                 process(x, y)
+    # scripts working on Windows cmd
     run('echo "" > tmp.dict', shell=True)
     run("hunalign -text -utf -realign tmp.dict pl.txt en.txt > aligned.txt", shell=True)
-    run("rm en.txt pl.txt tmp.dict translate.txt", shell=True)
+    run("del pl.txt en.txt tmp.dict translate.txt", shell=True)
 
 
 def process(pl, en):
     with open("pl.txt", mode="a", encoding="utf-8") as f:
         for l in sent_tokenize(pl["text"]):
-            f.write(l)
+            f.write(l + "\n")
     with open("en.txt", mode="a", encoding="utf-8") as f:
         for l in sent_tokenize(en["text"]):
-            f.write(l)
+            f.write(l + "\n")
 
 
 if __name__ == "__main__":

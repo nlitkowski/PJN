7dd5f3add0b157bf4850ba87fd64820a96ff5bfa
Norbert Litkowski <norbert.litkowski@gmail.com>
2019-12-16 10:47:26 +0100

    Add script for evaluation

--- filmweb/evaluate.py
+++ filmweb/evaluate.py
@@ -1,9 +1,10 @@
 from math import sqrt
+from sys import argv
 
 def main():
     # RMSE
-    with open("test_data_actual.txt") as f:
-        with open("test_preds.txt") as f2:
+    with open(argv[1]) as f:
+        with open(argv[2]) as f2:
             counter = 0
             average_error = 0.0
             for l1, l2 in zip(f, f2):
@@ -11,7 +12,7 @@ def main():
                 sec = float(l2)
                 counter += 1
                 average_error += ((sec - first)**2)
-            print(sqrt(average_error/counter))
+            print(f"RMSE = {sqrt(average_error/counter)}")
             
 
 if __name__ == '__main__':

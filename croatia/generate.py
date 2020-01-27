483d13b31903266a10f4e7b0fbe15389e7ebe1c5
nlitkowski <norbert.litkowski@gmail.com>
2020-01-27 02:33:14 +0100

    Aligned texts

--- croatia/generate.py
+++ croatia/generate.py
@@ -7,13 +7,13 @@ DELETE_FILES = False
 
 
 def main():
-    text_json = json.load(open("croatia/out4.json"))
+    text_json = json.load(open("out4.json"))
     if PROCESS_FILES:
         for x in text_json:
             process(x)
     # scripts working on Windows cmd
     run('echo "" > tmp.dict', shell=True)
-    run("hunalign -text -utf -realign tmp.dict hr.txt en.txt > aligned.txt", shell=True)
+    run("hunalign -utf tmp.dict en.txt hr.txt", shell=True)
     if DELETE_FILES:
         run("del hr.txt en.txt tmp.dict translate.txt", shell=True)
 
